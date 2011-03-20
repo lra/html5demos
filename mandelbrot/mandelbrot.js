575b5d76ed9d0e69afd8c6886727024e675a0aa5
Laurent Raufaste <laurent@freshplanet.com>
2011-03-20 19:30:59 -0400

    Working Mandelbrot

---
+++ mandelbrot/mandelbrot.js
@@ -0,0 +1,53 @@
+function draw()
+{
+	var canvas = document.getElementById('view');
+	var context = canvas.getContext('2d');
+	var pixels = context.createImageData(canvas.width, canvas.height);
+	var currentIteration = 1;
+	var maxIteration = 50;
+	var fps = 60;
+	var intval;
+
+	function computeColor(x, y, max_iteration)
+	{
+	    x = 2.5 * (x/canvas.width - 0.5);
+	    y = 2 * (y/canvas.height - 0.5);
+	    var x0 = x;
+	    var y0 = y;
+
+	    var iteration = 0;
+
+	    while (x * x + y * y <= 4 && iteration < max_iteration ) {
+	        var xtemp = x*x - y*y + x0;
+	        y = 2*x*y + y0;
+	        x = xtemp;
+	        iteration++;
+	    }
+
+	    return Math.round(255 * iteration / max_iteration);
+	}
+	
+	function render()
+	{
+		for (var x = 0; x < pixels.width; x++)
+		{
+		    for (var y = 0; y < pixels.height; y++)
+			{
+		        var color = computeColor(x, y, currentIteration);
+		        var idx = (x + y * canvas.width) * 4;
+		        pixels.data[idx + 0] = color;
+		        pixels.data[idx + 1] = color;
+		        pixels.data[idx + 2] = color;
+		        pixels.data[idx + 3] = 255;
+		    }
+		}		
+		context.putImageData(pixels, 0, 0);
+		currentIteration++;
+		if (currentIteration > maxIteration)
+		{
+			clearInterval(intval);
+		}
+	}
+	
+	intval = setInterval(render, 1000 / fps);
+}
