024abda883b501b396eba57cf6662db76695ef64
Laurent Raufaste <laurent@freshplanet.com>
2011-03-20 21:11:59 -0400

    Yellow working version

--- fire/fire.js
+++ fire/fire.js
@@ -4,11 +4,12 @@ function draw()
 	var context = canvas.getContext('2d');
 	var pixels = context.createImageData(canvas.width, canvas.height);
 	var currentIteration = 1;
-	var maxIteration = 10;
+	var maxIteration = 1000;
 	var fps = 60;
 	var intval;
 	var palette = Array();
 	var buffer = Array();
+	var bufferExtraHeight = 3;
 	
 	function setup()
 	{
@@ -18,14 +19,14 @@ function draw()
 			var rgb = new Object;
 			rgb.r = i;
 			rgb.g = i;
-			rgb.b = i;
+			rgb.b = 0;
 			palette[i] = rgb;
 		}
 		
 		// Initialize pixel buffer
 		for (var x = 0; x < canvas.width; x++)
 		{
-			for (var y = 0; y < canvas.width; y++)
+			for (var y = 0; y < canvas.height + bufferExtraHeight; y++)
 			{
 				setBufferColor(x, y, 0);
 			}
@@ -55,34 +56,34 @@ function draw()
 	{
 		var x;
 		var y;
-		for (x = 0; x < canvas.width; x++)
+		for (x = 25; x < canvas.width-25; x++)
 		{
 			var color;
 			color = Math.round(100 + 50 * Math.random());
-			setBufferColor(x, canvas.height - 1, color);
+			setBufferColor(x, canvas.height + bufferExtraHeight - 1, color);
 			color = Math.round(100 + 50 * Math.random());
-			setBufferColor(x, canvas.height - 2, color);
+			setBufferColor(x, canvas.height + bufferExtraHeight - 2, color);
 		}
 		var nb_blocs = 10 + Math.round(40 * Math.random());
 		for (var bloc = 0; bloc < nb_blocs; bloc++)
 		{
-			x = 1 + Math.round(Math.random() * (canvas.width - 2));
-			setBufferColor(x-1, canvas.height - 3, palette.length-1);
-			setBufferColor(x  , canvas.height - 3, palette.length-1);
-			setBufferColor(x+1, canvas.height - 3, palette.length-1);
-			setBufferColor(x-1, canvas.height - 2, palette.length-1);
-			setBufferColor(x  , canvas.height - 2, palette.length-1);
-			setBufferColor(x+1, canvas.height - 2, palette.length-1);
-			setBufferColor(x-1, canvas.height - 1, palette.length-1);
-			setBufferColor(x  , canvas.height - 1, palette.length-1);
-			setBufferColor(x+1, canvas.height - 1, palette.length-1);
+			x = 25 + Math.round(Math.random() * (canvas.width - 50));
+			setBufferColor(x-1, canvas.height - 3 + bufferExtraHeight, palette.length-1);
+			setBufferColor(x  , canvas.height - 3 + bufferExtraHeight, palette.length-1);
+			setBufferColor(x+1, canvas.height - 3 + bufferExtraHeight, palette.length-1);
+			setBufferColor(x-1, canvas.height - 2 + bufferExtraHeight, palette.length-1);
+			setBufferColor(x  , canvas.height - 2 + bufferExtraHeight, palette.length-1);
+			setBufferColor(x+1, canvas.height - 2 + bufferExtraHeight, palette.length-1);
+			setBufferColor(x-1, canvas.height - 1 + bufferExtraHeight, palette.length-1);
+			setBufferColor(x  , canvas.height - 1 + bufferExtraHeight, palette.length-1);
+			setBufferColor(x+1, canvas.height - 1 + bufferExtraHeight, palette.length-1);
 		}
 		for (x = 0; x < pixels.width; x++)
 		{
-		    for (y = 0; y < pixels.height - 2; y++)
+		    for (y = 0; y < pixels.height - 2 + bufferExtraHeight; y++)
 			{
 				var c = getBufferColor(x-1, y+1) + getBufferColor(x, y+1) + getBufferColor(x+1, y+1) + getBufferColor(x, y+2);
-				c = Math.round(c / 4 - 1);
+				c = Math.round(c / 4 - 2);
 				if (c < 0)
 				{
 					c = 0;
@@ -96,14 +97,7 @@ function draw()
 		    for (y = 0; y < pixels.height; y++)
 			{
 				c = getBufferColor(x, y);
-				try
-				{
-					setPixelRGB(x, y, palette[c].r, palette[c].g, palette[c].b);
-				}
-				catch (err)
-				{
-					console.log('Error'+c);
-				}
+				setPixelRGB(x, y, palette[c].r, palette[c].g, palette[c].b);
 			}
 		}
 		context.putImageData(pixels, 0, 0);
